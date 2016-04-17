17d89cbfca09df70537ed98a5dba87d612352cbb
Przemysław Matwiejczuk <matwiejczuk.przemyslaw@wp.pl>
2016-04-17 15:29:36 +0200

    Stable version add possibility to copy all files use only one click

--- src/MojeMetody/AllCpyOp.java
+++ src/MojeMetody/AllCpyOp.java
@@ -18,7 +18,7 @@ import javax.swing.JOptionPane;
 public class AllCpyOp {
     
     
-    ArrayList<String> fileList = new ArrayList<>();
+    
     
     String memory="";
     String memory1="";
@@ -86,68 +86,5 @@ public class AllCpyOp {
         
     }
     
-    
- /*   
-    public void add_arList_files(String arListFiles)
-    {   
-       
-     
-       //ArrayList<String> fileList = new ArrayList<>();
-       String files;
-       files = arListFiles;
-       memory += files;
-       fileList.add(memory); 
-       System.out.println("dodalem");
-       
-       
-       
-       
-       
-       
- 
-      
-       
-     
-   
-          
-        //System.out.println(place + where + files);
-       //System.out.println(place + where + files);
-    }
-    
-    public void add_path(String pathWhere, String pathPlace)
-    {  
-        place = pathPlace;
-        where = pathWhere;
-        System.out.println(place+where);
-    }
-    
-    public void show_files() 
-    {  
-        
-        for(String tmp:fileList)
-        {
-            memory1+=(tmp+"\n");
-        }
-    
-        
-         System.out.println("Wynik----------------------------------------------------------------------------");
-         System.out.println(memory1);
-         
-         
-      
-        
-        
-        
-        
-   
-      
-              
-         
-         
-         
-         
-        
-         
-    }
-    */
+
 }
